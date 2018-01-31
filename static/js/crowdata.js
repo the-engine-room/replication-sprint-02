135ab11f176296e77b29ab3a64c470f0e274cd91
root <root@engineroom1.koumbit.net>
2018-01-31 17:13:05 +0000

    modified on server

--- static/js/crowdata.js
+++ static/js/crowdata.js
@@ -27,18 +27,18 @@ jQuery.fn.formatTextMillons = function(length){ /* format numbers to $ 100 M */
                 n += (group1/1000000).toFixed(2) + " M";
             }else{
                 n = match;
-            }  
+            }
             return  n
         }));
   });
 };
 
-function replace_txt_html(){ // replace html_entities tags in "{{{TAGS}}}" 
+function replace_txt_html(){ // replace html_entities tags in "{{{TAGS}}}"
     if (!this.tags_txt) {
         this.find = "body :contains({{{)";
         this.tags_txt = $(this.find);
     }
-    
+
     if (this.tags_txt.length){
       var last_link = this.tags_txt.last();
       var html = last_link.html()
@@ -51,20 +51,20 @@ function replace_txt_html(){ // replace html_entities tags in "{{{TAGS}}}"
     }
 };
 
-    
+
 function create_sosial(cont_social, url, text, title){ // recibe the JQ content and url to share
     /* Exaple
     *
-    * create_sosial($('.herramientasSociales.social'), location.href, "Liberá un documento!");
+    * create_sosial($('.herramientasSociales.social'), location.href, "LiberÃ¡ un documento!");
     *
     */
-    
+
     if(!title) var title = document.title;
     if(!text) var text = title;
     if(!url) var url = location.href;
-    
+
     // FB
-    var face = $('<div class="col-md-3 fb-share-button facebookMeGusta" style="">'); 
+    var face = $('<div class="fb-share-button facebookMeGusta" style="width:84px; padding:0px; margin:0px;">');
         face.attr({
           "data-type": "box_count"
           // "data-href": url
@@ -81,30 +81,30 @@ function create_sosial(cont_social, url, text, title){ // recibe the JQ content 
           // "data-hashtags": "VozData"
         })
         .html('Tweet');
-    var wrap_tw = $('<div class="twitter col-md-3" style="">');
-        
+    var wrap_tw = $('<div class="twitter" style="width:65px; padding:0px; margin:0px;">');
+
     var twitter_js = $('<script src="http://platform.twitter.com/widgets.js">');
         wrap_tw.append(twitter);
 
     // GOO+
-    var gplus = $('<div class="google col-md-3" id="plusoneAcumulado" style="">');
+    var gplus = $('<div class="google" id="plusoneAcumulado" style="width:63px; padding:0px; margin:0px;">');
 
     // MAIL
-    var mail = $('<div class="compmail2 col-md-3" style="">');
+    var mail = $('<div class="compmail2" style="width:48px; padding-left:13px; margin:0px;">');
         mail.append(
           $('<a class="enviar bot_mail2" title="Enviar">').attr("href", "mailto:?subject=" + title +"&body=" + text + " " + url)
           // .append($('<img src="" border="0">'))
             );
-    
-    cont_social.html("").append(face, face_root, face_js, wrap_tw, twitter_js, gplus, mail );
-    
+
+    cont_social.html("").append("<b>Compartir</b>", face, face_root, face_js, wrap_tw, twitter_js, gplus, mail );
+
     gapi.plusone.render("plusoneAcumulado",{"size": "tall", "href": url});
 
   }
 
   /* Popup
   * var popup = new Popup();
-  * popup.show_p({ 
+  * popup.show_p({
   *     title: "Title of popup",
   *     body: "This is the content of body, it can be html",
   *     addclass: "class to add to '.popup_bg'"
@@ -132,15 +132,49 @@ function create_sosial(cont_social, url, text, title){ // recibe the JQ content 
       s.popup_cont.fadeIn('slow');
       if(callback) callback();
     });
+
+
+    $(".organization_buttons .cancel", this.popup_body).click(function() {
+        s.hide_p();
+    });
+
+      $(".organization_buttons .ok", this.popup_body).click(function() {
+          var organization_id = $(".organizations", this.popup_body).val();
+          var organization_slug = $(".organizations", this.popup_body).find(':selected').data('slug');
+
+          $.post("/cd/change_current_organization",
+               { organization_id : organization_id },
+                 function(response) {
+                     if(response.success) {
+                         if(response.organization_name == "none") {
+                             $(".organization_legend").hide();
+                             $(".organization_none_legend").fadeIn();
+                             $('.organization_none_legend .notification').fadeIn().delay(2000).fadeOut('slow');
+
+                         } else {
+                             $(".organization_none_legend").hide();
+                             $(".organization_legend").fadeIn();
+                             $(".organization_legend .organization_name").html(response.organization_name);
+                             $(".organization_legend .organization_name").data('slug', organization_slug);
+                             $('.organization_legend .notification').fadeIn().delay(2000).fadeOut('slow');
+                         }
+
+
+
+                         s.hide_p();
+                     } else {
+                         $(".error", this.popup_body).fadeIn();
+                     }
+               });
+    });
   }
+
   Popup.prototype.hide_p = function (callback){ // close popup
     var s = this;
     this.popup.fadeOut("fast", function(){
       s.popup_title.html(""); // set title
-      s.popup_body.html(""); // set body   
+      s.popup_body.html(""); // set body
       s.popup_cont.removeClass(s.addclass).hide();
       if(callback) callback();
     });
   }
-
-  /* /Popup */
